eb569f04c28da2ac09df8c7d6b5e890df6ad8714
Priyanka Pandey <pandey2@avaya.com>
2025-02-27 12:20:51 +0530

    first project

---
+++ test/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'test';
+  skills:Array<string> = ['JS','HTML','CSS'];
+  country: Array<string> = ["India","US","UK","JAPAN","CHINA"]
+}
